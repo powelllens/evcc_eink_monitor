ee139f4969d2d8d1dc0fc9f71bcd6d81fdbf0849
David Ruckes <david.ruckes@fst.com>
2022-08-14 18:01:10 +0200

    some minor changes and tests

--- src/config.h
+++ src/config.h
@@ -7,12 +7,10 @@
 #endif
 
 #define VERSION "1.1.0"
-//const char VERSION[] = "1.0.0";
 
 // Modifying the config version will probably cause a loss of the existig configuration.
 // Be careful!
 #define CONFIG_VERSION "1.1.0"
-//const char CONFIG_VERSION[] = "1.0.0";
 
 const char WIFI_AP_SSID[] = "EVCC E-Ink Monitor";
 const char WIFI_AP_DEFAULT_PASSWORD[] = "evccMonitor";
